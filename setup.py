c6742622e61094e78d99a27e95f966dc9dda0c57
Marcos Sánchez <arkanus@gmail.com>
2014-02-08 22:26:04 -0300

    Adds setup.py to package/install

---
+++ setup.py
@@ -0,0 +1,30 @@
+import sys
+from setuptools import setup
+
+setup(
+    name='selenium-sunbro',
+    version='0.01',
+    author='Marcos Sánchez',
+    author_email='arkanus@gmail.com',
+    description="Easily create page objects with a declarative syntax",
+    py_modules=['sunbro'],
+    url='https://github.com/arkanus/selenium-sunbro',
+    license='Mozilla Public License 2.0 (MPL 2.0)',
+    keywords='selenium wedriver page object',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=['selenium']
+)
